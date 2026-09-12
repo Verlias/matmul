37f22511c2b9b771226ce9c36cafb09a12b7b098
Verlias <loganvoravong@gmail.com>
2026-09-12 17:44:34 -0400

    Cache Tiling Implementation

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 hello:
-	g++ -O2 -std=c++17 main.cpp -o program
+	g++ -O3 -march=native -std=c++17 main.cpp -o program
 	./program
--- main.cpp
+++ main.cpp
@@ -46,9 +46,6 @@ Matrix naiveCacheFriendly(
   size_t k = v2[0].size();
   Matrix result(i, std::vector<int>(k, 0));
 
-  // loop order x, z, y is the cache-friendly one:
-  // v1[x][z] is a scalar (broadcast), v2[z][y] and result[x][y]
-  // are both walked row-wise (contiguous) as y increments
   for (size_t x = 0; x < i; ++x) {
     for (size_t z = 0; z < j; ++z) {
       for (size_t y = 0; y < k; ++y) {
@@ -56,6 +53,39 @@ Matrix naiveCacheFriendly(
       }
     }
   }
+  return result;
+}
+
+Matrix cacheTiling(
+  const Matrix& v1,
+  const Matrix& v2
+) {
+  size_t i = v1.size();
+  size_t j = v1[0].size();
+  size_t k = v2[0].size();
+  const size_t TileSize = 64;
+  Matrix result(i, std::vector<int>(k, 0));
+
+  for (size_t xx = 0; xx < i; xx += TileSize) {
+    for (size_t zz = 0; zz < j; zz += TileSize) {
+      for (size_t yy = 0; yy < k; yy += TileSize) {
+
+        size_t xEnd = std::min(xx + TileSize, i);
+        size_t zEnd = std::min(zz + TileSize, j);
+        size_t yEnd = std::min(yy + TileSize, k);
+
+        for (size_t x = xx; x < xEnd; ++x) {
+          for (size_t z = zz; z < zEnd; ++z) {
+            for (size_t y = yy; y < yEnd; ++y) {
+              result[x][y] += v1[x][z] * v2[z][y];
+            }
+          }
+        }
+
+      }
+    }
+  }
+
   return result;
 }
 
@@ -72,7 +102,7 @@ int main() {
   Matrix v1 = randomMatrix(N, N, 0, 9, 42);
   Matrix v2 = randomMatrix(N, N, 0, 9, 1337);
 
-  Matrix result1, result2;
+  Matrix result1, result2, result3;
 
   {
     auto start = std::chrono::high_resolution_clock::now();
@@ -90,8 +120,16 @@ int main() {
     std::cout << "naiveCacheFriendly time: " << duration.count() << " ms\n";
   }
 
+    {
+    auto start = std::chrono::high_resolution_clock::now();
+    result3 = cacheTiling(v1, v2);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::cout << "cacheTiling time: " << duration.count() << " ms\n";
+  }
+
   // sanity check: both implementations should agree
-  bool match = (result1 == result2);
+  bool match = (result1 == result3);
   std::cout << "Results match: " << (match ? "yes" : "no") << "\n";
 
   return 0;
